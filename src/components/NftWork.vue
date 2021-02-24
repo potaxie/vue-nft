91c017c7a84bf557367f5606badd5b0ba40ecd02
xiejianzhang <xiejianzhangmail@163.com>
2021-02-24 16:15:09 +0800

    init

--- src/components/NftWork.vue
+++ src/components/NftWork.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nft-title">Welcome Ginkgo</div>
-  <div v-for="group in groups" :key="group.name" style="margin: 12px 11%">
+  <div v-for="group in groups" :key="group.name" style="margin: 12px 12%">
     <a-row
       type="flex"
       justify="space-between"
