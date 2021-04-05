e4c5711df47ea3a8478708405b4a2ee6509b3e07
novicezk <zhukai341907@gmail.com>
2021-04-05 20:40:45 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,10 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       "/app": {
-        target: "http://172.20.10.4:50009",
+        // target: "http://172.20.10.4:50009",
         // target: "http://127.0.0.1:50009",
         // target: "https://www.ginkgonft.com",
-        // target: "http://192.168.31.100:50009",
+        target: "http://192.168.31.100:50009",
         // target: "http://192.168.0.103:50009",
       },
     },
